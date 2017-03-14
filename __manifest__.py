306cc755820e19c4c9fb451fdbdbfd4992b0e420
Mick <suncombo@gmail.com>
2017-03-14 17:31:20 +0800

    Add module

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2007-Now Mick Tseng(<suncombo@gmail.com>).
+
+{
+    'name': '台灣會計科目表',
+    'version': '1.0',
+    'category': 'Localization',
+    'author': 'suncombo@gmail.com',
+    'maintainer': 'suncombo@gmail.com',
+    'website': 'https://github.com/micktseng',
+    'description': """
+        台灣會計科目表
+    """,
+    'depends': ['l10n_tw'],
+    'data': [
+        'data/l10n_tw_chart_data.xml',
+        'data/account_chart_template_data.yml',
+    ],
+    'license': 'GPL-3',
+}
